0f0e9da7d9565f2daf3b3019a871d4785c93ee2f
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-11-16 16:53:20 +0100

    use texture path

--- crafter/env.py
+++ crafter/env.py
@@ -53,7 +53,7 @@ class Env(BaseClass):
     self._seed = seed
     self._episode = 0
     self._world = engine.World(area, constants.materials, (12, 12))
-    self._textures = engine.Textures(constants.root / 'minecraft')
+    self._textures = engine.Textures(constants.root / texture_path)
     item_rows = int(np.ceil(len(constants.items) / view[0]))
     self._local_view = engine.LocalView(
         self._world, self._textures, [view[0], view[1] - item_rows])
